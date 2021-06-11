b3aca95387bb4b3b3cc55c11c3c3cdbd524b1ce1
Linda Van Vooren <linda.vanvooren@gmail.com>
2021-06-11 09:49:33 -0500

    Update BookzyAppApplication.java
    update file

--- src/main/java/com/pied_piper/bookzy_app/BookzyAppApplication.java
+++ src/main/java/com/pied_piper/bookzy_app/BookzyAppApplication.java
@@ -2,6 +2,14 @@ package com.pied_piper.bookzy_app;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import javax.servlet.Servlet;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+import java.io.PrintWriter;
 
 @SpringBootApplication
 public class BookzyAppApplication {
@@ -10,4 +18,20 @@ public class BookzyAppApplication {
 		SpringApplication.run(BookzyAppApplication.class, args);
 	}
 
+}
+
+public class Hello extends HttpServlet implements Servlet {
+       
+    public Hello() {}
+
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+    throws ServletException, IOException
+    {
+        PrintWriter out = response.getWriter();
+        out.println("<html>");
+        out.println("<head><title>Servlet Example</title></head>");
+        out.println("<body>Hello from a Java Servlet</body>");
+        out.println("</html>");
+        out.close();
+    }
 }
